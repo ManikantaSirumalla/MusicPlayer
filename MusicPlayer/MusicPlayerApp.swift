e19a77b7052ff925fb9ece545942b339a70f5155
Manikanta Sirumalla <Manikanta.sirumalla@icloud.com>
2023-12-18 23:27:12 +0530

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Manikanta Sirumalla on 18/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
